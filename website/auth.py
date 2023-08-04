01d71aac0bef2d02025f2a2f3a46b63904c6ae00
HR-04 <harinirengarajan203@gmail.com>
2023-08-04 23:17:05 +0530

    updated functions in auth.py file

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,30 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
     return render_template("login.html")
 
-@auth.route('/logout')
+@auth.route('/logout',methods=['GET','POST'])
 def logout():
     return "<p>Logout</p>"
 
 @auth.route('/sign-up')
-def route():
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        
+
+
     return render_template("sign-up.html")
